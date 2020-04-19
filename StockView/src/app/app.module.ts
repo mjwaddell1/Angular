b7ed27d8848de93f4d3b6cfa0894e6b92f5932c7
Michael Waddell <35202179+mjwaddell1@users.noreply.github.com>
2020-04-19 01:21:07 -0400

    Update app.module.ts

--- StockView/src/app/app.module.ts
+++ StockView/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { StockMixComponent } from './stock-mix/stock-mix.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    GoogleChartsModule.forRoot()
+    GoogleChartsModule.forRoot(),
+    FormsModule
   ],
   providers: [DataFeedService],
   bootstrap: [AppComponent]
