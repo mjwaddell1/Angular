b5af7a2fc3289289819fd7d18984f3dd2ced64e6
Michael Waddell <35202179+mjwaddell1@users.noreply.github.com>
2020-04-19 01:14:19 -0400

    Update heading-bar.component.ts

--- StockView/src/app/heading-bar/heading-bar.component.ts
+++ StockView/src/app/heading-bar/heading-bar.component.ts
@@ -9,16 +9,30 @@ import { DataFeedService } from '../data-feed.service';
 export class HeadingBarComponent implements OnInit {
 
   public rg: number = this.feedsvc.range;
+  public showSettings: boolean = false;
+  public stks = 'xxx';
+  public range = 6;
+  public refresh = 0;
+  public baseline = 0;
 
   constructor(public feedsvc: DataFeedService) { this.rg = feedsvc.range; }
 
-  public updateData(moCnt) {
+  public updateData(rng) {
     console.log('updateData');
-    this.feedsvc.msgEvent.next(moCnt);
-    this.rg = moCnt;
+    this.feedsvc.msgEvent.next(rng);
+    this.rg = rng;
   }
 
   ngOnInit(): void {
+    this.stks = this.feedsvc.stocks.join(',');
+    this.range = this.feedsvc.range;
+    this.refresh = this.feedsvc.refreshRate;
+    this.baseline = this.feedsvc.baseline;
+  }
+
+  updateLink() {
+    document.location.href = document.location.pathname + '?range=' + this.range + '&refresh=' + this.refresh + '&baseline=' + this.baseline + '&stocks=' + this.stks;
+    this.showSettings = false;
   }
 
 }
